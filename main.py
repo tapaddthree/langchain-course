1f2b0bd3608ad45a2228935154efa2c111f794bd
Jakob <kalp@Jakobs-MacBook-Pro.local>
2026-04-19 16:46:11 -0500

    last commit for hello world

--- main.py
+++ main.py
@@ -20,7 +20,6 @@ Musk's political activities, statements and views have made him a polarizing fig
     Given the information {information} about a person I want you to create:
     1. A short summary
     2. Two interesting facts about them
-    Do not return markdown formatting, just plain text.
     """
     summary_prompt_template = PromptTemplate(input_variables=["information"], template=summary_template)
     llm = ChatOpenAI(
